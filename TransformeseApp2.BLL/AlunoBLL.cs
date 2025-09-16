4d6e9e7427fe33f00a207d39a547f7fee3b95587
GABRIEL MILONE VIEIRA <milonegabriel000@gmail.com>
2025-09-15 21:55:12 -0300

    Adição do código e estrutura do projeto

---
+++ TransformeseApp2.BLL/AlunoBLL.cs
@@ -0,0 +1,23 @@
+﻿using TransformeseApp2.DAL;
+using TransformeseApp2.DTO;
+
+namespace TransformeseApp2.BLL
+{
+    public class AlunoBLL
+    {
+        private List<AlunoDTO> _alunos = Database.Alunos;
+        public void CadastrarAluno(AlunoDTO alunoDTO)
+        {
+            //Validação antes de salvar o aluno
+            if (string.IsNullOrWhiteSpace(alunoDTO.Nome))
+                throw new Exception("Nome do aluno é obrigatório.");
+            Database.Alunos.Add(alunoDTO);
+        }
+        public List<AlunoDTO> ListarAlunos() => Database.Alunos;
+
+        public AlunoDTO GetById(int id)
+        {
+            return _alunos.FirstOrDefault(a => a.Id == id);
+        }
+    }
+}
