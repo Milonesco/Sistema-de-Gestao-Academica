185dcad346e3fb86f9dee7ef4a5896893204dd89
GABRIEL MILONE VIEIRA <milonegabriel000@gmail.com>
2025-10-16 21:52:40 -0300

    feat(unidade): ajusta manipulação da lista de unidades

--- TransformeseApp2.BLL/UnidadeBLL.cs
+++ TransformeseApp2.BLL/UnidadeBLL.cs
@@ -8,15 +8,20 @@ namespace TransformeseApp2.BLL
         private List<UnidadeDTO> _unidades = Database.Unidades;
         public void CadastrarUnidade(UnidadeDTO unidadeDTO)
         {
+            var unidade = Database.Unidades;
+
             //Validação antes de salvar a unidade
             if (string.IsNullOrWhiteSpace(unidadeDTO.Nome))
                 throw new Exception("Nome da unidade é obrigatório.");
-                Database.Unidades.Add(unidadeDTO);
+
+            unidade.Add(unidadeDTO);
+            Database.Unidades = unidade; // Salva a lista no JSON
         }
         public List<UnidadeDTO> ListarUnidades() => Database.Unidades;
 
         public void AtualizarUnidade(UnidadeDTO unidadeDTO)
         {
+            var unidades = Database.Unidades;
             var unidadeExistente = Database.Unidades.FirstOrDefault(unidade => unidade.Id == unidadeDTO.Id);
             if (unidadeDTO== null)
                 throw new Exception("Unidade não encontrada.");
@@ -28,10 +33,12 @@ namespace TransformeseApp2.BLL
             // Atualiza os campos do aluno
             unidadeExistente.Nome = unidadeDTO.Nome;
             unidadeExistente.Endereco = unidadeDTO.Endereco;
+            Database.Unidades = unidades; // Salva a lista atualizada no JSON
         }
 
         public void RemoverUnidade(int id)
         {
+            var unidades = Database.Unidades;
             var unidade= Database.Unidades.FirstOrDefault(unidade=> unidade.Id == id);
             if (unidade== null)
             {
@@ -39,6 +46,7 @@ namespace TransformeseApp2.BLL
             }
 
             Database.Unidades.Remove(unidade);
+            Database.Unidades = unidades; // Salva a lista atualizada no JSON
         }
 
         public UnidadeDTO GetById(int id)
