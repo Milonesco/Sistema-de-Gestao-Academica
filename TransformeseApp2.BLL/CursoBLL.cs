e17034db62c38af2d869eb9e5be1ee1725c38292
GABRIEL MILONE VIEIRA <milonegabriel000@gmail.com>
2025-10-16 21:51:39 -0300

    feat(curso): Corrige persistência e lógica de alterações

--- TransformeseApp2.BLL/CursoBLL.cs
+++ TransformeseApp2.BLL/CursoBLL.cs
@@ -8,17 +8,23 @@ namespace TransformeseApp2.BLL
         private List<CursoDTO> _cursos = Database.Cursos;
         public void CadastrarCurso(CursoDTO cursoDTO)
         {
+            var curso = Database.Cursos;
+
             //Validação antes de salvar o curso
             if (string.IsNullOrWhiteSpace(cursoDTO.Nome))
                 throw new Exception("Nome do curso é obrigatório.");
             if (int.IsNegative(cursoDTO.CargaHoraria))
                 throw new Exception("Carga horaria precisa ser positiva, tente novamente, por favor!");
-            Database.Cursos.Add(cursoDTO);
+            
+
+            curso.Add(cursoDTO);
+            Database.Cursos = curso; // Salva a lista no JSON
         }
         public List<CursoDTO> ListarCursos() => Database.Cursos;
 
         public void AtualizarCurso(CursoDTO cursoDTO)
         {
+            var cursos = Database.Cursos;
             var cursoExistente = Database.Cursos.FirstOrDefault(curso => curso.Id == cursoDTO.Id);
             if (cursoExistente == null)
                 throw new Exception("Curso não encontrado.");
@@ -31,6 +37,7 @@ namespace TransformeseApp2.BLL
             cursoExistente.Nome = cursoDTO.Nome;
             cursoExistente.Id = cursoDTO.Id;
             cursoExistente.CargaHoraria = cursoDTO.CargaHoraria;
+            Database.Cursos = cursos; // Salva a lista atualizada no JSON
         }
 
 
@@ -38,6 +45,7 @@ namespace TransformeseApp2.BLL
 
         public void RemoverCurso(int id)
         {
+            var cursos = Database.Cursos;
             var curso = Database.Cursos.FirstOrDefault(curso => curso.Id == id);
             if (curso == null)
             {
@@ -45,6 +53,7 @@ namespace TransformeseApp2.BLL
             }
 
             Database.Cursos.Remove(curso);
+            Database.Cursos = Database.Cursos; // Salva a lista atualizada no JSON
         }
         public CursoDTO GetById(int id)
         {
