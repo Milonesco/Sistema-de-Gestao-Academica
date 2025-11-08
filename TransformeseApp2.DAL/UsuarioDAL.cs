c07b6578e80749a453cd4a4b1fa4971c7a7c9c44
GABRIEL MILONE VIEIRA <milonegabriel000@gmail.com>
2025-11-07 21:54:30 -0300

    feat(usuario): adiciona autenticação com SQL Server

---
+++ TransformeseApp2.DAL/UsuarioDAL.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Data.SqlClient;
+using TransformeseApp2.DTO;
+
+namespace TransformeseApp2.DAL
+{
+    public class UsuarioDAL : Conexao
+    {
+        public UsuarioDTO Autenticar(string login, string senha)
+        {
+			try
+			{
+				Conectar();
+				command = new SqlCommand("SELECT * FROM Usuario WHERE Login = @Login AND Senha = @Senha", conexao);
+
+                command.Parameters.AddWithValue("@Login", login);
+                command.Parameters.AddWithValue("@Senha", senha);
+                dataReader = command.ExecuteReader();
+
+                UsuarioDTO usuario = null;
+                if(dataReader.Read())
+                {
+                    {
+                        usuario = new();
+                        usuario.Login = dataReader["Login"].ToString();
+                        usuario.Senha = dataReader["Senha"].ToString();
+                        usuario.TpUsuario = dataReader["TpUsuario"].ToString();
+                    }
+                }
+                return usuario;
+            }
+			catch (Exception erro)
+			{
+				throw new Exception(erro.Message);
+			}
+        }
+    }
+}
