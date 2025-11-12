0d90a464111a7a1d1862f9e13f19e4fe4118bcfc
GABRIEL MILONE VIEIRA <milonegabriel000@gmail.com>
2025-11-11 21:43:26 -0300

    feat(usuario): substitui acesso direto ao banco por UsuarioDAL

--- TransformeseApp2.BLL/UsuarioBLL.cs
+++ TransformeseApp2.BLL/UsuarioBLL.cs
@@ -5,66 +5,81 @@ namespace TransformeseApp2.BLL
 {
     public class UsuarioBLL
     {
-        public UsuarioDTO? Login(string Login, string Senha)
+        UsuarioDAL usuarioDAL = new();
+        public UsuarioDTO? Login(string login, string senha)
         {
-            var usuario = Database.Usuarios.FirstOrDefault(u => u.Login == Login && u.Senha == Senha);
+            //var usuario = Database.Usuarios.FirstOrDefault(u => u.Usuario == Login && u.Senha == Senha);
 
-            if (usuario == null)
-            {
-                throw new Exception("Usuário ou senha inválidos");
-            }
+            //if (usuario == null)
+            //{
+            //    throw new Exception("Usuário ou senha inválidos");
+            //}
 
 
-            return usuario;
+            //return usuario;
+            return usuarioDAL.Autenticar(login, senha);
+        }
+        public List<TipoUsuarioDTO> GetTipoUsuario()
+        {
+            return usuarioDAL.GetTipoUsuario();
         }
 
-        public List<UsuarioDTO> ListarUsuarios() => Database.Usuarios;
+        public List<UsuarioDTO> ListarUsuarios()
+        {
+            return usuarioDAL.Listar();
+        }
 
         public void AtualizarUsuario(UsuarioDTO usuarioDTO)
         {
-            var usuarioExistente = Database.Usuarios.FirstOrDefault(usuario => usuario.Id == usuarioDTO.Id);
-            if (usuarioExistente == null)
-                throw new Exception("Usuario não encontrado.");
+            //var usuarioExistente = Database.Usuarios.FirstOrDefault(usuario => usuario.Id == usuarioDTO.Id);
+            //if (usuarioExistente == null)
+            //    throw new Exception("Usuario não encontrado.");
+
+            //if (string.IsNullOrWhiteSpace(usuarioDTO.Nome))
+            //    throw new Exception("Nome é obrigatório.");
+
+
+            //// Atualiza os campos do usuario
+            //usuarioExistente.Id = usuarioDTO.Id;
+            //usuarioExistente.Nome = usuarioDTO.Nome;
+            //usuarioExistente.Usuario = usuarioDTO.Usuario;
+            //usuarioExistente.Senha = usuarioDTO.Senha;
+            //usuarioExistente.UrlFoto = usuarioDTO.UrlFoto;
 
             if (string.IsNullOrWhiteSpace(usuarioDTO.Nome))
                 throw new Exception("Nome é obrigatório.");
 
-
-            // Atualiza os campos do usuario
-            usuarioExistente.Id = usuarioDTO.Id;
-            usuarioExistente.Nome = usuarioDTO.Nome;
-            usuarioExistente.Login = usuarioDTO.Login;
-            usuarioExistente.Senha = usuarioDTO.Senha;
-            usuarioExistente.UrlFoto = usuarioDTO.UrlFoto;
+            usuarioDAL.Update(usuarioDTO);
         }
 
         public void CadastrarUsuario(UsuarioDTO usuario)
         {
-            var usuarios = Database.Usuarios;
+            //var usuarios = Database.Usuarios;
 
-            if (string.IsNullOrWhiteSpace(usuario.Nome))
-            {
-                throw new Exception("Nome é obrigatório!");
-            } 
+            //if (string.IsNullOrWhiteSpace(usuario.Nome))
+            //{
+            //    throw new Exception("Nome é obrigatório!");
+            //} 
             
-            if (string.IsNullOrWhiteSpace(usuario.Login))
-            {
+            //if (string.IsNullOrWhiteSpace(usuario.Usuario))
+            //{
 
-                throw new Exception("Login é obrigatório!");
-            }            
+            //    throw new Exception("Login é obrigatório!");
+            //}            
             
-            if (string.IsNullOrWhiteSpace(usuario.Senha))
-            {
-                throw new Exception("Senha é obrigatório!");
-            }
+            //if (string.IsNullOrWhiteSpace(usuario.Senha))
+            //{
+            //    throw new Exception("Senha é obrigatório!");
+            //}
 
-            usuarios.Add(usuario);
-            Database.Usuarios = usuarios; // Salva a lista no JSON
+            //usuarios.Add(usuario);
+            //Database.Usuarios = usuarios; // Salva a lista no JSON
+            usuarioDAL.Create(usuario);
         }
 
         public void RedefinirSenha(string login, string novaSenha)
         {
-            var usuario = Database.Usuarios.FirstOrDefault(u => u.Login == login);
+            var usuario = Database.Usuarios.FirstOrDefault(u => u.Usuario == login);
             if (usuario == null)
                 throw new Exception("Usuário não encontrado!");
 
