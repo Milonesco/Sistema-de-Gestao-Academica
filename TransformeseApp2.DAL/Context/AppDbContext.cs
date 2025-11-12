b6d39674043527f6b43d28d7980adbe5996ff6f6
GABRIEL MILONE VIEIRA <milonegabriel000@gmail.com>
2025-11-11 21:51:54 -0300

    feat(DAL): adiciona classe AppDbContext para EF Core

---
+++ TransformeseApp2.DAL/Context/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TransformeseApp2.DAL.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+    }
+}
